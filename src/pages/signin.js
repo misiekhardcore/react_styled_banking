d5796fe153805198c079563714608b5a7fc3eaf7
misiekhardcore <misiekhardcore@gmail.com>
2020-12-07 11:43:55 +0100

    added scroll to top on load

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,8 +1,14 @@
 import React from "react";
+import ScrollToTop from "../components/ScrollToTop";
 import Signin from "../components/Signin";
 
 const SigninPage = (props) => {
-  return <Signin />;
+  return (
+    <>
+      <ScrollToTop />
+      <Signin />
+    </>
+  );
 };
 
 export default SigninPage;
